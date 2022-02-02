68a3b0cbabb90d65221cd8e654fa83835b8ae8b1
LorenzoTorelli <lorenzo.torelli98@gmail.com>
2022-02-02 15:50:48 +0100

    init

---
+++ laravel-comics/resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.layoutBase')
+
+@section('pageTitle')
+    Home
+@endsection
+
+@section('pageContent')
+    <h1>hello World</h1>
+@endsection
---
+++ laravel-comics/resources/views/partials/header.blade.php
@@ -0,0 +1,22 @@
+<header class="header">
+    <div class="container">
+        <div id="img-box"><img src="../assets/img/dc-logo.png" alt="dc-logo" ></div>
+        <div id="nav-box">
+            <nav>
+                <ul>
+                   <li><a href="">Characters</a></li>
+                   <li><a href="">Comics</a></li>
+                   <li><a href="">Movies</a></li>
+                   <li><a href="">Tv</a></li>
+                   <li><a href="">Games</a></li>
+                   <li><a href="">Collectibles</a></li>
+                   <li><a href="">Videos</a></li>
+                   <li><a href="">Fans</a></li>
+                   <li><a href="">News</a></li>
+                   <li><a href="">Shop</a></li>
+
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
